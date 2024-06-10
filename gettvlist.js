265b1937d673cc532f3dff6dd3123751ba4f961a
ElvisZhu <zjb1229@gmail.com>
2024-06-10 16:58:30 +0800

    Create gettvlist.js

---
+++ gettvlist.js
@@ -0,0 +1,13 @@
+#http://tonkiang.us/hotellist.html
+
+var s = '#EXTM3U'; 
+for (let i = 0; i < rs.length; i++) {
+    if (rs[i].children.length>1) {
+        var name= rs[i].children[0].innerText.replaceAll('\t','').replaceAll('\n','')
+        var m3u8 = rs[i].children[1].innerText.replaceAll('\t','').replaceAll('\n','')
+        if (name!='') {
+            s = s+'\n#EXTINF:0,'+name+'\n'+m3u8
+        }
+    }
+}
+console.log(s)
